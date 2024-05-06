8f5051f279af45f2409d63c676de37ecf597081b
MUHAMMAD SYAIFULLAH <mahkotonagata@gmail.com>
2024-05-06 20:55:56 +0700

    <edit>Scanresult

--- tugas_project4_giziqu/lib/user/Scanresult.dart
+++ tugas_project4_giziqu/lib/user/Scanresult.dart
@@ -10,125 +10,181 @@ class Scanresult extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Kembali'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              color: Colors.white,
-              child: Column(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Produk 1'),
-                      Text('Informasi Gizi'),
-                      Container(
-                        height: 400,
-                        width: 350,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(18),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        // Menggunakan SingleChildScrollView di sini
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Produk 1',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                        child: ListView.builder(
-                          itemCount: 12,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Text('Test'),
-                              onTap: () {
-                                // Aksi ketika item diklik
-                                print('Tapped on ');
-                              },
-                            );
-                          },
+                        Text(
+                          'Informasi Nilai Gizi',
+                          style: TextStyle(
+                            fontSize: 22,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: 350,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Ringkasan Gizi"),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 20),
-                        width: 160,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(18),
+                        SizedBox(height: 10),
+                        Container(
+                          height: 400,
+                          width: 350,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          child: ListView.builder(
+                            itemCount: 12,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text('Test'),
+                                onTap: () {
+                                  // Aksi ketika item diklik
+                                  print('Tapped on ');
+                                },
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                      Container(
-                        width: 160,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                      )
-                    ],
-                  ),
-                  Text("Produk Terkait"),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 20),
-                        width: 160,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                      ),
-                      Container(
-                        width: 160,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Scanresult()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
+                      ],
                     ),
-                  ),
-                  child: Text('Bandingkan'),
+                  ],
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 10),
+              Container(
+                width: 350,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Ringkasan Gizi",
+                        style: TextStyle(
+                          fontSize: 22,
+                        )),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 20),
+                          width: 165,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        ),
+                        Container(
+                          width: 165,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Text("Produk Terkait",
+                        style: TextStyle(
+                          fontSize: 22,
+                        )),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 20),
+                          width: 165,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          child: Stack(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(
+                                    10.0), // Ubah sesuai kebutuhan
+                                child: Container(
+                                  width: 500,
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                    color: Colors
+                                        .blue, // Ubah warna sesuai kebutuhan
+                                    borderRadius: BorderRadius.circular(18),
+                                  ),
+                                  child: Icon(Icons.photo, color: Colors.white),
+                                ),
+                              ),
+                              // Judul
+                              Positioned(
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                child: Container(
+                                  padding: EdgeInsets.all(8),
+                                  child: Text(
+                                    'Produk Lain',
+                                    style: TextStyle(color: Colors.white),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: 165,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Scanresult()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: Text('Bandingkan'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
