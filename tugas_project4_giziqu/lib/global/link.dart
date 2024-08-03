d266270e4f3dd1c266026b044ee0b76079c65053
ahmad.fauzy <ahmad.fauzy@rollingglory.com>
2024-08-03 11:47:51 +0700

    finioshing aplikasi

--- tugas_project4_giziqu/lib/global/link.dart
+++ tugas_project4_giziqu/lib/global/link.dart
@@ -1,3 +1,3 @@
 // ignore_for_file: non_constant_identifier_names
-String link = "https://cc4f-2404-c0-2e10-00-411a-9dbe.ngrok-free.app/";
+String link = "https://web-be-giziqu.site/";
 String link_yolo = "https://84c5-2404-c0-2e10-00-411c-4efe.ngrok-free.app/";
